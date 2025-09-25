5b5d89afbafe486f4c7585cadf70cb8968608038
mgbarro <72791756+mgbarro@users.noreply.github.com>
2025-09-25 20:25:53 +0200

    Fixing nav links references

--- scripts/activesection.js
+++ scripts/activesection.js
@@ -1,15 +1,19 @@
-// Obtener todos los enlaces de la navbar
-  const navLinks = document.querySelectorAll('.nav-links a');
-  const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-links a');
+const sections = document.querySelectorAll('section');
 
-  navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault(); // Evitar el salto instantáneo
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // Evitar el salto instantáneo
 
-      //const targetId = link.getAttribute('href').substring(1); // elimina el #
+    // Actualizar clase "active" en la navbar
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
 
-      // Actualizar clase "active" en la navbar
-      navLinks.forEach(l => l.classList.remove('active'));
-      link.classList.add('active');
-    });
+    // Desplazarse a la sección correspondiente
+    const targetId = link.getAttribute('href').substring(1); // elimina el #
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
   });
+});
