3945ce1171dab00e91728fb01540aa28a9e21990
mgbarro <72791756+mgbarro@users.noreply.github.com>
2025-09-25 17:58:38 +0200

    Updating proyect links

--- scripts/activesection.js
+++ scripts/activesection.js
@@ -1,12 +1,12 @@
 // Obtener todos los enlaces de la navbar
-  const navLinks = document.querySelectorAll('.navbar a');
+  const navLinks = document.querySelectorAll('.nav-links a');
   const sections = document.querySelectorAll('section');
 
   navLinks.forEach(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault(); // Evitar el salto instantáneo
 
-      const targetId = link.getAttribute('href').substring(1); // elimina el #
+      //const targetId = link.getAttribute('href').substring(1); // elimina el #
 
       // Actualizar clase "active" en la navbar
       navLinks.forEach(l => l.classList.remove('active'));
