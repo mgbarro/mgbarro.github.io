d33f83fd71cdf458623c4e0ac4805cddc8a3d67a
mgbarro <72791756+mgbarro@users.noreply.github.com>
2026-04-01 15:40:36 +0200

    [TEST] Adding new Home background and UI fixes

--- scripts/toggledarkmode.js
+++ scripts/toggledarkmode.js
@@ -6,8 +6,10 @@ toggleBtn.addEventListener('click', () => {
   // Guardar preferencia en localStorage para que se mantenga al recargar
   if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('theme', 'dark');
+    toggleBtn.classList.replace('fa-moon', 'fa-sun')
   } else {
     localStorage.setItem('theme', 'light');
+    toggleBtn.classList.replace('fa-sun', 'fa-moon')
   }
 });
 
